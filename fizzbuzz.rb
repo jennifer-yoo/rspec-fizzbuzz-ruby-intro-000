722bf4b4a6e34a65d17d4c112f8b39a54ad67eb4
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-02-19 02:12:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(value)
-  if value % 3 == "Fizz"
-  elsif value % 5 == "Buzz"
-  elsif value % 15 == "FizzBuzz"
-  elsif value % 4 == nil
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 15 == 0
+    "FizzBuzz"
+  elsif int % 4 == nil
 end
